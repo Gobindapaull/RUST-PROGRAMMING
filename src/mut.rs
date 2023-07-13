977f039f19f4582f1c594fb887939b5a0ccc6bd2
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-07-13 13:48:59 +0530

    Create mut.rs

---
+++ src/mut.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut my_num = 123;
+    println!("my number is {}", my_num);
+
+    my_num = 456;
+    println!("my number changed to {}", my_num);
+}
+
+
+// mutability = liability or tendency to change
+
+// immutable = unchangeable/fixed/constant
