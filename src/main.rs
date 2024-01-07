487adc74539306ff9f7d55bafbcad86a87638cba
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-01-07 10:01:09 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,17 @@
+pub mod helper;
+
+fn main() {
+    println!("Hello, world!");
+    test_fun();
+    let store_name = helper::get_full_name("crypto", "millionaire");
+    println!("main: {}", store_name);
+
+    let add_sum = helper::helper_child::sum(99);
+    println!("sum is {}", add_sum);
+}
+
+#[allow(dead_code)]
+fn test_fun() {
+    println!("waiting ... ");
+}
 
