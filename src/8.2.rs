daf1242730941bbfcfbfdb0a88e92ea74929fb4e
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-01-29 22:46:50 +0530

    Create 8.2.rs

---
+++ src/8.2.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut s = String::new();
+    println!("{s}");
+
+    let data = "some string data";
+    let t = data.to_string();
+    println!("{t}");
+
+    let o = String::from("text data");
+    println!("{o}");
+    
+}
