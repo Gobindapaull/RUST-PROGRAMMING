e95be1b12b6562bada8147ed152f5ff89d288adb
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-08-27 04:32:53 +0530

    Create match.rs

---
+++ src/match/match.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let user_id = 12345;
+
+    match user_id {
+        1234 => println!("matched"),
+        234 => println!("small matched"),
+        ________________________________________ => println!("not matched")
+    }
+}
