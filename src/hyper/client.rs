4edbdea20f313a50a3a67f7958c96b851eaa8af9
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-12-14 22:13:43 +0530

    Update client.rs

--- src/hyper/client.rs
+++ src/hyper/client.rs
@@ -2,8 +2,9 @@ use hyper::{ Client, Uri};
 
 #[tokio::main]
 async fn main() {
-    let client = Client::new();
-    let uri = "http://www.google.com".parse::<Uri>().unwrap();
+    let client = Client::new(); // to create a new http client
+    let uri = "http://www.google.com".parse::<Uri>().unwrap(); // turbo fish syntax with type Uri from hyper.
     let result = client.get(uri).await.unwrap();
+    // get method on client to sent a request to the uri
     println!("{:?}", result.status());
 }
