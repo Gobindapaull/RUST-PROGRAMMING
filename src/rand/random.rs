7ab38ea87592312251d208b6357a62e1a14ddcc4
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-07-24 10:12:21 +0530

    Update random.rs

--- src/rand/random.rs
+++ src/rand/random.rs
@@ -6,8 +6,7 @@ pub fn run() {
         let x: u32 = rand::random();
         println!("Random number: {x}");
 
-        let mut rng = rand::rng();
-        let number = rng.random_range(100..200);
+        let number = rand::rng().random_range(100..200);
         println!("Random number between 100 and 200 : {number}");
 
         println!("---------------------------------------------------------");
